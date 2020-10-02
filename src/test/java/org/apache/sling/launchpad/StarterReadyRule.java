47417411b37792386b1092aa5b50fc2ee8f964a8
Robert Munteanu <rombert@apache.org>
2020-10-02 15:02:31 +0200

    SLING-9637 - Re-enable Smoke IT, but only keep the repository available check
    Naming changes, mostly from launchpad to starter

--- src/test/java/org/apache/sling/launchpad/StarterReadyRule.java
+++ src/test/java/org/apache/sling/launchpad/StarterReadyRule.java
@@ -29,14 +29,14 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.junit.rules.ExternalResource;
 
-public class LaunchpadReadyRule extends ExternalResource {
+public class StarterReadyRule extends ExternalResource {
 
     private static final int TRIES = 60;
     private static final int WAIT_BETWEEN_TRIES_MILLIS = 1000;
 
     private final List<Check> checks = new ArrayList<>();
 
-    public LaunchpadReadyRule(int launchpadPort) {
+    public StarterReadyRule(int launchpadPort) {
 
         checks.add(new Check("http://localhost:" + launchpadPort + "/server/default/jcr:root/content"));
         checks.add(new Check("http://localhost:" + launchpadPort + "/content/starter.html") {
@@ -93,7 +93,7 @@ public class LaunchpadReadyRule extends ExternalResource {
             Thread.sleep(WAIT_BETWEEN_TRIES_MILLIS);
         }
         
-        throw new RuntimeException(String.format("Launchpad not ready. Failed check for URL %s with message '%s'",
+        throw new RuntimeException(String.format("Starter not ready. Failed check for URL %s with message '%s'",
                 check.getUrl(), lastFailure));
     }
 
