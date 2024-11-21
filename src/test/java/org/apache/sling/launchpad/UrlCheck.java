5ab5316187f8fcc4e3f56d0007fc1372eb3d491d
Stefan Seifert <stefanseifert@users.noreply.github.com>
2024-11-21 15:18:56 +0100

    SLING-12503 apply spotless code formatting

--- src/test/java/org/apache/sling/launchpad/UrlCheck.java
+++ src/test/java/org/apache/sling/launchpad/UrlCheck.java
@@ -1,18 +1,20 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 package org.apache.sling.launchpad;
 
@@ -46,8 +48,9 @@ class UrlCheck {
     }
 
     /** Run all supplied checks */
-    static void runAll(CloseableHttpClient client, int retryCount, int msecBetweenRetries, Collection<UrlCheck> checks) throws Exception {
-        for(UrlCheck check : checks) {
+    static void runAll(CloseableHttpClient client, int retryCount, int msecBetweenRetries, Collection<UrlCheck> checks)
+            throws Exception {
+        for (UrlCheck check : checks) {
             String lastFailure = null;
             HttpGet get = new HttpGet(check.getUrl());
             int requestCount = 0;
@@ -60,27 +63,23 @@ class UrlCheck {
                         Thread.sleep(msecBetweenRetries);
                         continue;
                     }
-    
+
                     lastFailure = check.run(response);
                     if (lastFailure == null) {
                         break;
                     }
-                } catch ( ConnectException e ) {
+                } catch (ConnectException e) {
                     lastFailure = e.getClass().getName() + " : " + e.getMessage();
                 }
-    
+
                 Thread.sleep(msecBetweenRetries);
             }
-            
-            if(lastFailure != null) {
-                throw new RuntimeException(
-                    String.format("Starter not ready or path not found after %d tries (%d msec). Request to URL %s failed with message '%s'",
-                    requestCount, 
-                    System.currentTimeMillis() - start,
-                    check.getUrl(), 
-                    lastFailure));
+
+            if (lastFailure != null) {
+                throw new RuntimeException(String.format(
+                        "Starter not ready or path not found after %d tries (%d msec). Request to URL %s failed with message '%s'",
+                        requestCount, System.currentTimeMillis() - start, check.getUrl(), lastFailure));
             }
         }
     }
-
 }
