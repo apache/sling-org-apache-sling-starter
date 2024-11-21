0837cf5e1b9a6464ea1accf724b0c595d693c1e3
Stefan Seifert <stefanseifert@users.noreply.github.com>
2024-11-21 15:18:56 +0100

    SLING-12503 apply spotless code formatting

--- src/test/java/org/apache/sling/launchpad/StarterReadyRule.java
+++ src/test/java/org/apache/sling/launchpad/StarterReadyRule.java
@@ -1,18 +1,20 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 package org.apache.sling.launchpad;
 
@@ -40,9 +42,11 @@ public class StarterReadyRule extends ExternalResource {
         checks.add(new UrlCheck(baseURL, "/server/default/jcr:root/content"));
         checks.add(new UrlCheck(baseURL, "/content/starter.html") {
             final String READY_MARKER = "Do not remove this comment, used for Starter integration tests";
+
             @Override
             public String run(HttpResponse response) throws Exception {
-                try (InputStreamReader isr = new InputStreamReader(response.getEntity().getContent());
+                try (InputStreamReader isr =
+                                new InputStreamReader(response.getEntity().getContent());
                         BufferedReader reader = new BufferedReader(isr)) {
 
                     String line;
@@ -61,18 +65,19 @@ public class StarterReadyRule extends ExternalResource {
     @Override
     protected void before() throws Throwable {
         final Throwable previous = previousFailures.get(launchpadPort);
-        if(previous != null) {
+        if (previous != null) {
             throw new RuntimeException(
-                String.format("%s failed previously on port %d, refusing to run more tests", getClass().getSimpleName(), launchpadPort),
-                previous
-            );
+                    String.format(
+                            "%s failed previously on port %d, refusing to run more tests",
+                            getClass().getSimpleName(), launchpadPort),
+                    previous);
         }
         final int TRIES = 60;
         final int WAIT_BETWEEN_TRIES_MILLIS = 1000;
         try (CloseableHttpClient client = HttpClients.createDefault()) {
             try {
                 UrlCheck.runAll(client, TRIES, WAIT_BETWEEN_TRIES_MILLIS, checks);
-            } catch(Throwable t) {
+            } catch (Throwable t) {
                 previousFailures.put(launchpadPort, t);
                 throw t;
             }
