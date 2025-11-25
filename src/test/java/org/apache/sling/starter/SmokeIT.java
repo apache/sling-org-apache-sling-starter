59542035df4fb2620494373f5af99e1ebb3e9098
Stefan Seifert <stefanseifert@users.noreply.github.com>
2025-11-25 10:38:57 +0100

    SLING-12623 rename packages org.apache.sling.launchpad.* to org.apache.sling.starter.*

--- src/test/java/org/apache/sling/starter/SmokeIT.java
+++ src/test/java/org/apache/sling/starter/SmokeIT.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.sling.launchpad;
+package org.apache.sling.starter;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
